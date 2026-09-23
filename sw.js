8b7ce58f4d7f8519c277a2c1b10de1928e78c91a
levigmgaming-commits <levi.gm.gaming@gmail.com>
2026-09-23 17:15:25 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='artiman-ai-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const clone=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return resp}).catch(()=>caches.match('./index.html'))))});
